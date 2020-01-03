0285b8358c99c3e0344a8df35c0f52e5624dec17
Fei Wang <feiwangsioc@gmail.com>
2020-01-03 02:26:10 +0000

    finish 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,21 @@ class User < ApplicationRecord
   has_many :groups
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  
+   has_many :group_relationships
+   has_many :participated_groups, :through => :group_relationships, :source => :group
+   
+     def is_member_of?(group)
+       participated_groups.include?(group)
+     end
+     
+  def join!(group)
+    participated_groups << group
+  end
+
+  def quit!(group)
+    participated_groups.delete(group)
+  end
+  
 end
